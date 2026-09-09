b8ab3be981c6675d18b1832924641d0109faff89
uvais8958 <uvaisa740@gmail.com>
2026-09-09 17:30:52 +0530

    start journey dsa with java javaBasic

---
+++ javaBasics.java
@@ -0,0 +1,55 @@
+// boiler plate code
+
+public class javaBasics {
+
+    // main function
+    public static void main(String args[]){
+
+        // for output
+        // System.out.print("Hello world\n");      //line Breake
+        // System.out.print("Hello world\n");       //line breake
+        // System.out.print("Hello world\n");
+
+
+        //print a pettern
+        // System.out.println("****");
+        // System.out.println("***");
+        // System.out.println("**");
+        // System.out.println("*");
+
+
+
+        // varriable
+        // int a=10;
+        // int b=20;
+        // System.out.println(b);//20
+        // System.out.println(a);//10
+        // String name="Uvais";
+        // System.out.println(name);//Uvais
+
+        // a=202; 
+
+        // b=a;
+        // System.out.println(a);//202
+        // System.out.print(b);//202
+
+        // data types primitive
+        // byte b=20;
+        // System.out.println(b);;//20
+        // char ch='a';
+        // System.out.println(ch);//a
+        // boolean var=true;
+        // System.out.println(var);//ture
+        // float price=10;
+        // System.out.println(price);//10.0
+        // int number=123;
+        // System.out.println(number);//123
+        // //long
+        // //double
+        // short n=240;
+        // System.out.println(n);//240
+
+
+
+    }
+}
