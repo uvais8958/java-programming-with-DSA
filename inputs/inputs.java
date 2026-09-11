96eca6918de70405f9408bfa171e5ce70bd810ba
uvais8958 <uvaisa740@gmail.com>
2026-09-11 10:31:53 +0530

    inputs in java prectice

---
+++ inputs/inputs.java
@@ -0,0 +1,42 @@
+package inputs;
+
+import java.util.Scanner;
+import java.util.*;
+
+public class inputs {
+    public static  void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        // String input=sc.next();
+        // System.out.print(input);//Uvais
+      
+        // String name=sc.nextLine();
+        // System.out.print(name);  //Muhammad Uvais
+
+        // float price=sc.nextFloat();
+        // System.out.print(price);//10.0
+
+        // double c=sc.nextFloat();
+        // System.out.print(c);//10.19999
+
+        // boolean m=sc.nextBoolean();
+        // System.out.print(m);//true
+         
+
+        // int a=sc.nextInt();
+        // int b=sc.nextInt();
+        // int sum=a+b;
+        // System.out.print("Sum=" + sum);// 10 + 10 = sum=20
+
+        // int  a=sc.nextInt();
+        // int b=sc.nextInt();
+        // int product=a*b;
+        // System.out.print(product);  // 2*8 =16
+           
+        //Area of circle
+        // float rad=sc.nextFloat();
+        // float  area=3.14f * rad *rad;
+        // System.out.print(area);  // 2 = 12.56
+
+    }
+    
+}
