2c8bab689f5e9c50395a9a3b5e2eed72d01a05ed
uvais8958 <uvaisa740@gmail.com>
2026-09-20 10:34:02 +0530

    binary to decimal and decimal to binary convert by function

--- functions/isPrime.java
+++ functions/isPrime.java
@@ -38,8 +38,51 @@ public static boolean isPrime(int n){
     }
     return  true;
 }
+
+
+
+public static void primeInRange(int n){
+    for(int i=2;i<=n;i++){
+        if(isPrime(i)){
+            System.out.print(i+" ");
+        }
+    }
+     System.out.println();
+}
+//convert to binary to decimal
+public static void bintoDec(int binNum){
+   int myNum=binNum;
+    int pow=0;
+    int decNum=0;
+    while(binNum>0){
+        int lastDigit=binNum%10;
+        decNum=decNum+(lastDigit*(int)Math.pow(2,pow));
+        pow++;
+        binNum=binNum/10;
+    }
+    System.out.print("decimal of " + myNum +" = " +decNum);
+}
+
+//convert to decimal to binary
+public static void decToBinery(int n){
+   int myNum=n;
+    int pow=0;
+    int binNum=0;
+    while(n>0){
+        int rem=n%2;
+        binNum=binNum+(rem * (int)Math.pow(10,pow));
+        pow++;
+        n=n/2;
+         
+    }
+    System.out.print("binary number form of="+myNum+"="+binNum);
+
+}
 public static void main (String args[]){
-        System.out.print(isPrime(12));//false
+  // primeInRange(20);//2 3 5 7 9 11 13 17 19
+   //bintoDec(1010);
+   decToBinery(10);//1010
+    // System.out.print(isPrime(12));//false
 
     }
     
